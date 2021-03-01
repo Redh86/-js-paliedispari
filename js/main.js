7b82681fd3507982519cfc0a519008dbceaa74ed
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-03-01 09:39:57 +0100

    secondo esercizio incompleto

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 
-function palindtrome(wrds) {
-    var lowRegStr = wrds.toLowerCase();
-    var reverseStr = lowRegStr.split('').reverse().join('');
-    if (reverseStr === lowRegStr) {
-        return true
-    }
-    return false
+// function palindtrome(wrds) {
+//     var lowRegStr = wrds.toLowerCase();
+//     var reverseStr = lowRegStr.split('').reverse().join('');
+//     if (reverseStr === lowRegStr) {
+//         return true
+//     }
+//     return false
+// }
+
+
+// prl = prompt("Inserisci una parola")
+// if (palindtrome(prl)){
+//     alert("la parola è palindroma")
+// } else {
+//     alert("la parola non è palindroma")
+// }
+
+
+
+
+
+function button(){
+    var nbr = parseInt(document.getElementById("num").value);
+    var parDis = document.getElementsByName('scelta').value;
+    console.log(nbr);
+    console.log(parDis); 
+    var somma = nbr + Math.floor(Math.random()*10 - 5);
+    console.log(somma);
 }
 
-
-prl = prompt("Inserisci una parola")
-if (palindtrome(prl)){
-    alert("la parola è palindroma")
-} else {
-    alert("la parola non è palindroma")
+if (somma % 3 == 0 && document.getElementById('dispari').value) {
+    alert("Hai vinto")
 }
